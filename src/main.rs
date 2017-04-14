4e2c09c01c8ff16f723a1f8c9eb22ace6d97df0d
Willy Aguirre <marti1125@gmail.com>
2017-04-14 11:28:06 -0500

    add templates and statics

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+use std::io;
+use std::path::{Path, PathBuf};
+
+extern crate rocket_contrib;
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
+use rocket_contrib::Template;
+use rocket::response::NamedFile;
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+#[get("/test")]
+fn test() -> &'static str {
     "Hello, World!"
 }
 
+#[get("/")]
+fn index() -> Template {
+    let context = ();
+    Template::render("index", &context)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![files, index, test]).launch();
 }
