5057bd7351adb87f844afa55413682960be6c780
Willy Aguirre <marti1125@gmail.com>
2017-04-22 11:30:12 -0500

    add momeent and show next event at home page

---
+++ static/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $.get('http://localhost:8000/events', function(data){
+    $.each(data, function(k, v) {
+      $('#events').append('<h3>'+v.name+'</h3><h4>'+convertDate(v.date)+'</h4><p>'+v.about+'</p>');
+    })
+  });
+
+  function convertDate(date){
+    return moment(date).format('DD/MM/YYYY hh:mm A');
+  }
+
+});
