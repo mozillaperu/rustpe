59f4d6f7fb8deb18bfb8000c7e65733b336f3ebb
Willy Aguirre <marti1125@gmail.com>
2017-04-29 00:29:45 -0500

    update event

--- src/model/event.rs
+++ src/model/event.rs
@@ -7,22 +7,26 @@ pub struct Event {
     id: i32,
     name: String,
     about: String,
-    date: chrono::NaiveDateTime
+    date: chrono::NaiveDateTime,
+    url: String,
+    slide: String
 }
 
 impl Event {
 
     pub fn all(pool: Pool) -> Vec<Event> {
         let selected_events: Vec<Event> =
-        pool.prep_exec("SELECT id, name, about, date from heroku_c0585ee816e7fb3.event", ())
+        pool.prep_exec("SELECT id, name, about, date, url, slide from heroku_c0585ee816e7fb3.event", ())
         .map(|result| {
             result.map(|x| x.unwrap()).map(|row| {
-                let (id, name, about, date) = my::from_row(row);
+                let (id, name, about, date, url, slide) = my::from_row(row);
                 Event {
                     id: id,
                     name: name,
                     about: about,
-                    date: date
+                    date: date,
+                    url: url,
+                    slide: slide
                 }
             }).collect()
         }).unwrap();
--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 
-  $.get('http://rustpe.org/events', function(data){
+  $.get('http://rustpe.org/events', function(data) {
     $.each(data, function(k, v) {
-      $('#events').append('<h3>'+v.name+'</h3><h4>'+convertDate(v.date)+'</h4><p>'+v.about+'</p>');
+      $('#events').append(v.name, convertDate(v.date), v.about, v.slide);
     })
   });
 
-  function convertDate(date){
+  function eventTemplate(name, date, about, slide) {
+    return '<h3>'+v.name+'</h3>' +
+           '<h4>'+convertDate(v.date)+'</h4>' +
+           '<p>'+v.about+'</p>' +
+           '<p><a href="'+slide+'" target="_blank">Presentación</a>';
+  }
+
+  function convertDate(date) {
     return moment(date).format('DD/MM/YYYY hh:mm A');
   }
 
